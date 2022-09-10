25920871b7368165f8a73ff38922db8411b9fcc3
gautamsen117 <gautamsen117@gmail.com>
2022-09-11 02:04:02 +0600

    summary

---
+++ summary.js
@@ -0,0 +1,6 @@
+var destination = ['sajek', 'coxsbazar', 'nepal', 'bali', 'singapur'];
+console.log(destination[2]);
+destination[2] = 'thailand';
+var index = destination.indexOf('bhali');
+destination.push('bhutan');
+destination.pop();
