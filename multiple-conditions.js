ff09fbb055486833dc759db0ce7d8951dab3433b
gautamsen117 <gautamsen117@gmail.com>
2022-09-11 00:25:20 +0600

    multiple-conditon

---
+++ multiple-conditions.js
@@ -0,0 +1,40 @@
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
+
+// if(isGraduated == true){
+//     console.log('Eso biye kory feli');
+// }
+// else{
+//     console.log('Tor kopale biya nai');
+// }
+
+
+// if(isGraduated == true && salary > 50000){
+//     console.log('Eso biye kore feli');
+// }
+// else{
+//     console.log('Tor kopale biya nai');
+// }
+
+
+// if(isGraduated == true && salary > 50000 && cars >= 1){
+//     console.log('Eso biye kore feli');
+// }
+// else{
+//     console.log('Tor kopale biya nai');
+// }
+
+var isGraduated = false;
+var salary = 75000;
+var cars = 1;
+
+if(isGraduated === true || salary > 50000 || cars >= 1){
+    console.log('Eso prem kori');
+}
+else{
+    console.log('tomar friend er number dao');
+}
+
+
+// if(isGraduated === true && (salary > 5000 || cars >= 1))
