913adfaad1a9bc960e4a79fb008e1773d0de7fe2
gautamsen117 <gautamsen117@gmail.com>
2022-09-11 00:39:10 +0600

    multi stage condition

---
+++ multi-stage-conditon.js
@@ -0,0 +1,17 @@
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBuscuit = 20;
+
+if(danishPrice < money){
+    console.log('Dan dan danish khabo');
+}
+else if(butterBread < money){
+    console.log('Butter bread dan amak');
+}
+else if(toastBuscuit < money){
+    console.log('toast dan khuda lagche');
+}
+else {
+    console.log('Khali tea ai dan r kichu lagbe na')
+}
