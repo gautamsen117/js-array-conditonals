cce0ea672c6117f6620e0457f0b103df07bd556d
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 22:17:13 +0600

    index

---
+++ index.js
@@ -0,0 +1,12 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+//get elements value by index
+var elements = numbers[2];
+// console.log(elements);
+
+// set element value by index
+numbers[1] = 77;
+numbers[3] = 44;
+// console.log(numbers);
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
