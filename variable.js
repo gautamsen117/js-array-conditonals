54a8513a5ae202d339a21f96214df8da93ec6747
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 21:24:28 +0600

    recap

---
+++ variable.js
@@ -0,0 +1,32 @@
+// number type variable
+var sunglassPrice = 120;
+
+// string type variable
+var specialOneName = "Gautam Sen";
+
+// boolean type variable
+var isSingle = true;
+
+// two types of number: integer: 120;
+// float, decimal: 3.65;
+
+// variable naming convention: rules, best practice
+/**
+ * 1. single word
+ * 2. no gap or space
+ * 3. no quote
+ * 4. can not start with a number
+ * 5. can not contain dash
+ * 6. should use camel case
+*/
+
+// math operation: +, -, * , /
+// shorthand: +=, -=, *=, /=, ++, --
+// convert: parseInt(), parseFloat()
+
+
+//toFixed()
+
+//typeof
+
+// remainder: vagses %
