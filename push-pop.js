abf714deb2134e79ec9e9dc7e1268f75506f2e22
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 23:01:14 +0600

    push-pop

---
+++ push-pop.js
@@ -0,0 +1,16 @@
+var numbers = [78, 45, 98, 45];
+
+// use push to add element to an array as the last element array
+numbers.push(63);
+// console.log(numbers);
+
+var friends = ['balam', 'kalam', 'salam'];
+friends.push('gelam');
+// console.log(friends);
+
+// use pop to last element
+var number = [12, 13, 15, 16, 35];
+number.pop();
+var elementNumber = number.pop();
+console.log(number);
+console.log(elementNumber);
