30b1cba15a179afa7a1ace86f9e62e9dbde61516
gautamsen117 <gautamsen117@gmail.com>
2022-09-10 23:31:29 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,27 @@
+// console.log(5 < 6);
+// console.log(6 < 5);
+// console.log(5 > 6);
+// console.log(6 > 5);
+// console.log(5 <= 6);
+// console.log(15 <= 15);
+// console.log(16 <= 15);
+// console.log(5 != 5);
+// console.log(6 != 5);
+// console.log(5 >= 7);
+// console.log(7 >= 6);
+// console.log(13 == 13);
+// console.log(12 == 13);
+
+var num1 = 56;
+var num2 = 89;
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+
+
+// multiple condition
+// money1 > money2 && result1 > result2 && height1 > height2
+// money1 > money2 || result1 > result2 || height1 > height2
