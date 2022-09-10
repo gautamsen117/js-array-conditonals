b48465099cb97ae131e73ffc533b6878e1854d94
gautamsen117 <gautamsen117@gmail.com>
2022-09-11 00:52:56 +0600

    nested conditions

---
+++ nested-conditions.js
@@ -0,0 +1,16 @@
+var math = true;
+var geometry = true;
+var chemistry = false;
+
+if(math = true){
+    console.log('you are very good');
+    if(geometry = true){
+        console.log('you are good');
+    }
+    else{
+        console.log('you are bad');
+    }
+}
+else{
+    console.log('you are not good');
+}
