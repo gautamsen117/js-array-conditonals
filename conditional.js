d664173ddc1350cb93a9a89eb1a6632de8866aad
gautamsen117 <gautamsen117@gmail.com>
2022-09-11 00:04:58 +0600

    condition

---
+++ conditional.js
@@ -0,0 +1,35 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+
+// if iphone is less than my budget. i will buy the phone
+
+/**
+ * if iphone is less than my budget
+ *      i will buy the iphone
+ * 
+ * if (condition){
+ *           // will execute if the condition is true
+ * }
+ * 
+ */
+if (iphonePrice < myBudget){
+    // console.log('ami iphone kinbo');
+}
+
+
+var chickenPrice = 500;
+var myMoney = 50;
+// if (chickenPrice <= myMoney){
+//     console.log('Wow! chicken khabo Hurry.......!!!');
+// }
+
+// if(chickenPrice > myMoney){
+//     console.log('Smashed potato and lentils shoup');
+// }
+
+if (chickenPrice < myMoney){
+    console.log('Wow! chicken khabo');
+}
+else{
+    console.log('i eat potato');
+}
