d04a632932bb33da204ad3382707f47c1eb7575e
gautamsen117 <gautamsen117@gmail.com>
2022-09-11 12:07:20 +0600

    practice problem one

---
+++ practice-problem-one.js
@@ -0,0 +1,15 @@
+var fruit = ['Apple', 'Banana', 'Orange'];
+// i will try find index of Banana
+console.log(fruit[1]);
+// now i will try to replace Banana with Mango
+fruit[1] = 'Mango';
+// print replace output
+console.log(fruit);
+// now i will try to remove last element from array 
+fruit.pop();
+// without last element 
+console.log(fruit);
+// now add a new element 
+fruit.push('Watermelon');
+// print with watermelon
+console.log(fruit);
