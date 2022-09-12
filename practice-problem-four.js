66ce600a801edc186abac517158e4d29d8d975b6
gautamsen117 <gautamsen117@gmail.com>
2022-09-12 15:34:19 +0600

    practice problem four

---
+++ practice-problem-four.js
@@ -0,0 +1,9 @@
+// You are given a triangle with the sides 9, 8, 9. Write a program to check wether a tringle is Isosceles or not using if-else.
+// Isoscles => two sides are equal.
+
+var side1 = 9;
+var side2 = 8; 
+var side3 = 9;
+if(side1 == side2 || side1 == side3 || side2 == side3){
+    console.log('Isosceles');
+}
